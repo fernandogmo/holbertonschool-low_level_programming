f86500bba6770aa376431c5f73218afdcc7e7b03
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-14 21:19:37 +0000

    Added 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int huns, tens, ones;
+
+	for (huns = '0'; huns <= '7'; huns++)
+	{
+		tens = huns + 1;
+		while (tens <= '8')
+		{
+			ones = tens + 1;
+			while (ones <= '9')
+			{
+				putchar(huns);
+				putchar(tens);
+				putchar(ones++);
+				
+				if (huns != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			tens++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
