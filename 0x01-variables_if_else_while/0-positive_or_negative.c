153ef650d62bc6d213b8e65d2b9b580da8931ffd
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-14 04:44:06 +0000

    Added token doc comment for main 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
