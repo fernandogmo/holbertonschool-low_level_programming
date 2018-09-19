20fec6bd8f420eab9b044a62c67ad71a3eda69d9
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-19 06:43:30 +0000

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * add - returns the sum of two input ints
+  * @a - first input int
+  * @b - second input int
+  * 
+  * Returns - int
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
