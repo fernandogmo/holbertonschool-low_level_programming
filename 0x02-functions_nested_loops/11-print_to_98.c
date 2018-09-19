e5c663936ab99bde8ac51743c345509046511584
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-19 07:05:33 +0000

    Added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * print_to_98 - returns the sum of two input ints
+  * @n: first input int
+  *
+  * Return: Null
+  */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		(n > 98) ? n-- : n++;
+	}
+	printf("98\n");
+}
