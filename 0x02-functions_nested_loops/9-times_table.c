974e6766cde9dd68ad72e2d2924a96c151a8746a
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-19 17:40:01 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,18 +14,27 @@ void times_table(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			ab = a * b;
+			if (b == 0)
+			{
+				_putchar(0 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				ab = a * b;
+				
+				tens = ab > 9 ? ab / 10 : -16;
+				_putchar(tens + '0'); /* -16 + '0' is a SPACE */
 
-			tens = ab > 9 ? ab / 10 : -16;
-			_putchar(tens + '0'); /* -16 + '0' is a SPACE */
+				ones = ab % 10;
+				_putchar(ones + '0');
 
-			ones = ab % 10;
-			_putchar(ones + '0');
-
-			if (b == 9)
-				break;
-			_putchar(',');
-			_putchar(' ');
+				if (b == 9)
+					break;
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
