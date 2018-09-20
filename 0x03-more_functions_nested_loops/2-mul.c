61ae91d7bf7ca5b6baf9fc3d6bb19aa45586a8ec
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-20 21:46:10 +0000

    Added 2-mul.c

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  * mul - returns the product of two input ints
+  * @a: first input int
+  * @b: second input int
+  *
+  * Return: integer product of a & b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
