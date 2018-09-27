1c84d062a7804f61961e16019876d2f02286ebce
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-27 02:13:54 +0000

    Added a rough 100-atoi.c to try on the checker

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _atoi - takes a pointer to a string and converts it to
+ *         an integer.
+ * @s: pointer to string.
+ *
+ * Return: signed int
+ */
+int _atoi(char *s)
+{
+	int p;
+	unsigned int ret = 0;
+
+	while ((*s != '\0') && !(*s >= '0' && *s <= '9'))
+		if (*s++ == '-')
+			p *= -1;
+	while ((*s != '\0') && (*s >= '0' && *s <= '9'))
+		ret = ret * 10 + (*s++ - '0');
+
+	return (p * ret);
+}
