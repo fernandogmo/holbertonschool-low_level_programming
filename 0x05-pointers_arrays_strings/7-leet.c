1f24476e4bbe9849be6e1a8445ea1790eee02a27
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-09-28 20:30:29 +0000

    Added 7-leet.c

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words in a string.
+ * @a: pointer to a string
+ * Return: pointer to modified string
+ */
+char *leet(char *a)
+{
+	int i, j;
+	const char *codes[5] = {"aA4", "eE3", "oO0", "tT7", "lL1"};
+
+	for (i = 0; a[i]; i++)
+		for (j = 0; j < 5; j++)
+			if (a[i] == codes[j][0] || a[i] == codes[j][1])
+				a[i] = codes[j][2];
+
+	return (a);
+}
