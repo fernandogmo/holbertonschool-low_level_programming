4c4fc5005030ae041346b475be932fee97926624
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-02 02:35:36 +0000

    Added 4-strpbrk.c

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - locates the first occurrence in the string s of
+ *	      any of the bytes in the string accept.
+ * @s: pointer to a string
+ * @accept: string containing acceptable bytes
+ * Return: pointer to the byte in s that matches one of the bytes
+ *         in accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j] && s[i] != accept[j]; j++)
+			;
+		if (accept[j])
+			break;
+	}
+
+	return (s + i);
+}
