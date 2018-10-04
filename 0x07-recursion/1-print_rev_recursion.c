45477770591b980083317c2301d8a05925891869
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-04 05:17:49 +0000

    Added 1-print_rev_recursion.c

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string in reverse.
+ * @s: pointer to a char.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
