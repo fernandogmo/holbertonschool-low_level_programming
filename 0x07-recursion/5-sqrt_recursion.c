88e285d48c544eb6fd06b7c1867afe7e4b16fb53
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-05 00:55:06 +0000

    Added 5-sqrt_recursion.c

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion_aux - returns the natural square root of a number.
+ * @n: integer.
+ * @guess: integer, guess at sqrt(n).
+ * Return: integer sqrt of n or -1 if n is not a perfect square.
+ */
+int _sqrt_recursion_aux(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	else
+		return (_sqrt_recursion_aux(n, ++guess));
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: integer.
+ * Return: integer sqrt of n or -1 if n is not a perfect square.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return(_sqrt_recursion_aux(n, 0));
+}
+
