674edc8746ef19489a55abd7dcb2766e0db4be4f
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-05 03:42:06 +0000

    Fixed long line in 6-is_prime_number.c

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,8 +1,8 @@
 /**
- * is_prime_aux - does the actual work of checking if n is prime
- * @n: input integer to check
- * @i: factor to check against
- * Return: 1 if factor reaches n, calls itself with inc i, 0 if not prime
+ * is_prime_aux - recursive primality test used by is_prime_number.
+ * @n: integer to primality test
+ * @i: factor to check
+ * Return: integer 1 if prime, 0 otherwise
  */
 int is_prime_aux(int n, int i)
 {
@@ -15,7 +15,8 @@ int is_prime_aux(int n, int i)
 
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ *                   otherwise return 0.
  * @n: integer to primality test.
  * Return: integer 1 if prime, 0 otherwise.
  */
