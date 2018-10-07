08bd94c15384fae14f569d11d59277fcd5317660
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-07 23:01:47 +0000

    Added 0-whatsmyname.c

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints its executable name, followed by \n
+ * Return: 0 on SUCCESS
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
