d7edb2a1c4d60ffeafc2e6b67fbca4fd3278dd85
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-07 23:07:58 +0000

    Fixed betty-doc for 0-whatsmyname.c

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 
 /**
  * main - prints its executable name, followed by \n
+ * @argc: integer number of command-line arguments
+ * @argv: array containing command-line arguments
  * Return: 0 on SUCCESS
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
