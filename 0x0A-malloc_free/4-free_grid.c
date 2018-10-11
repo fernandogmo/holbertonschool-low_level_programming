e44f8c2f174cd347199efc413d0d227980d94d6d
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-11 05:09:25 +0000

    Fixed betty for 4-free_grid.c

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -2,7 +2,9 @@
 #include "stdlib.h"
 
 /**
- * free_grid - frees a 2d-array of ints previously created with *	      alloc_grid.
+ * free_grid - frees a 2d-array of ints previously created with
+ *	       alloc_grid.
+ * @grid: pointer to 2d-array of ints
  * @height: integer for rows in grid
  */
 void free_grid(int **grid, int height)
