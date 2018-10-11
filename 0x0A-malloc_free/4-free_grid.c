a73e9939ca550b88ed818f87972fd050abb0049e
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-11 05:06:39 +0000

    Added 4-free_grid.c

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include "stdlib.h"
+
+/**
+ * free_grid - frees a 2d-array of ints previously created with *	      alloc_grid.
+ * @height: integer for rows in grid
+ */
+void free_grid(int **grid, int height)
+{
+	while (--height >= 0)
+		free(grid[height]);
+
+	free(grid);
+}
