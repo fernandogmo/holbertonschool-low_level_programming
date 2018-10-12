241480ab1cdd77f4422479d98c4122d85d3bdd68
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-12 21:10:15 +0000

    Freed input pointer (if reallocated) in 100-realloc.c

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -1,18 +1,24 @@
 #include <stdlib.h>
 
 /**
- *
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to reallocate
+ * @old_size: original size of ptr memory block
+ * @new_size: new size of ptr memory block
+ * Return: pointer to reallocated memory, or NULL if malloc
+ *	   fails or new_size is 0
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ret = ptr;
-	char *tmp = ret;
+	char *tmp;
 
 	if (!new_size)
 		free(ret);
 
 	if (new_size > old_size)
 	{
+		tmp = ptr;
 		ret = malloc(new_size);
 		if (ret)
 			while (old_size--)
