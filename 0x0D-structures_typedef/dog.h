34f98827956ffae43fa0923a45a4ae0f0ad954c8
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-16 06:54:59 +0000

    Added new_dog to dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,13 +7,15 @@
  * @age: float
  * @owner: string pointer
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
