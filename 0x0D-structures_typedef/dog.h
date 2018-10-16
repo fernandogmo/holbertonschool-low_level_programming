950b53c183cb6843c81eb5c82bb2fea912d91810
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-16 04:21:52 +0000

    Added dog.h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - Holberton doggie structure
+ * @name: string pointer
+ * @age: float
+ * @owner: string pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
