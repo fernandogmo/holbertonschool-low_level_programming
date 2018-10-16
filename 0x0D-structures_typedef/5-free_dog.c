b675eb7ab645152223086ad5f8f84af3124c25c2
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-16 21:43:31 +0000

    Added 5-free_dog.c

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: a dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
