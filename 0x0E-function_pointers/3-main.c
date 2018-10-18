cfe6fa8497efd7c13c2d44069ab3898a67bd95ef
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-18 06:44:30 +0000

    Added 3-main.c

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs arithmetic operations on two numbers
+ * @argc: integer number of commandline arguments
+ * @argv: string array of commandline arguments
+ *
+ * Return: integer result of the operation, or exit with error code on failure.
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b;
+	char op;
+
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	op = argv[2][0];
+	a = atoi[1];
+	b = atoi[3];
+
+	if (b == 0 && (op == '/' || op == '%'))
+	{
+		puts("Error");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(argv[2])(a, b));
+
+	return (0);
+}
