d87fe865109cb2b3b9374a408310dce9957ec525
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-18 02:48:26 +0000

    Added 0-print_name.c

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - prints a name with a given function
+ * @name: string pointer
+ * @f: pointer to a void function that takes a string
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
