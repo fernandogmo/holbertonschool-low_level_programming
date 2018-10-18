b0a4a5e27d5f3c0c8e8076bb0421fc3795be0ded
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-18 18:45:41 +0000

    Refactored 3-get_op_func.c to use strcmp and NULL op in ops[]

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function to perform the
@@ -22,9 +23,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (ops[i].op[0] == *s)
+		if (!strcmp(ops[i].op, s))
 			return (ops[i].f);
 		i++;
 	}
