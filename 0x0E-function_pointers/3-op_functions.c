dda9f37f5ca2432ca41911f19714f9ce1c5318f5
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-18 06:42:32 +0000

    Added 3-op_functions.c

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+
+/**
+ * @a: integer
+ * @b: integer
+ * Return: integer sum of a & b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * @a: integer
+ * @b: integer
+ * Return: integer difference of a minus b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * @a: integer
+ * @b: integer
+ * Return: integer product of a & b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * @a: integer
+ * @b: integer
+ * Return: integer floor division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * @a: integer
+ * @b: integer
+ * Return: integer a mod b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
