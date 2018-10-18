5c0337dda4a87a6572bf6c56ea637dac81867f90
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-18 06:40:12 +0000

    Added 3-calc.h

---
+++ 0x0E-function_pointers/3-calc.h
@@ -0,0 +1,25 @@
+#ifndef __3_CALC_H__
+#define __3_CALC_H__
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+/* Functions in 3-op_functions.c */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/* Functions in 3-get_op_func.c */
+int (*get_op_func(char *s))(int, int);
+
+#endif
