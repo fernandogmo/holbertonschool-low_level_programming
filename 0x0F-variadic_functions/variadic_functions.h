5faa267f7561ee3d5a393fa63cdb0b84dbdc6034
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-19 22:22:23 +0000

    Fixed typo  ->  in variadic_functions.h

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef __VARIADIC_FUNCTIONS_H__
+#define __VARIADIC_FUNCTIONS_H__
+/* Function for 0-sum_them_all.c */
+int sum_them_all(const unsigned int n, ...);
+
+/* Function for 1-print_numbers.c */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/* Function for 2-print_strings.c */
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/* Function for 3-print_all.c */
+void print_all(const char * const format, ...);
+
+#endif
