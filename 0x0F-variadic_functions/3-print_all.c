7dd7619ca1cce572a17afd87b084258d64051704
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-20 02:34:59 +0000

    Fixed betty for 3-print_all.c

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -3,10 +3,10 @@
 
 /**
  * print_all - prints a variable number of parameters, of any combination
- *	       of char, int, float or string, using a string of the
- *	       characters 'c', 'i', 'f', and 's', to specify the
- *	       types of the arguments as char, int, float or
- *	       string, respectively.
+ *	        of char, int, float or string, using a string of the
+ *	        characters 'c', 'i', 'f', and 's', to specify the
+ *	        types of the arguments as char, int, float or
+ *	        string, respectively.
  * @format: constant string specifying types
  */
 void print_all(const char * const format, ...)
@@ -18,25 +18,26 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	while (format && format[i])
 	{
-		switch(format[i]) {
-			case 'c':
-				 printf("%c", (char)va_arg(args, int));
-				 break;
-			case 'i':
-				 printf("%i", va_arg(args, int));
-				 break;
-			case 'f':
-				 printf("%f", (float)va_arg(args, double));
-				 break;
-			case 's':
-				 str = va_arg(args, char *);
-				 if (!str)
-					 str = "(nil)";
-				 printf("%s", str);
-				 break;
-			default:
-				 i++;
-				 continue;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", (char)va_arg(args, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", (float)va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (!str)
+				str = "(nil)";
+			printf("%s", str);
+			break;
+		default:
+			i++;
+			continue;
 		}
 		if (format[++i])
 			printf(", ");
