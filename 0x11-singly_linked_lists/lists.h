74667e475034cc384ff6a15e12e842649f46d91a
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-26 04:21:13 +0000

    Added lists.h

---
+++ 0x11-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef __LISTS_H__
+#define __LISTS_H__
+
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif /* __LISTS_H__ */
