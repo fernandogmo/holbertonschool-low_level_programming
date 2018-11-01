41c6e2cf24961567854489a632f78b610db208e9
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-11-01 07:18:56 +0000

    Refactored for posterity

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,4 @@
 #include "lists.h"  /* listint_t */
-#include <stddef.h> /* ptrdif_t */
 
 /**
   * print_listint_safe - can print listint_t linked lists with loops.
@@ -8,35 +7,19 @@
   */
 size_t print_listint_safe(const listint_t *head)
 {
-	int count = 0, a;
-	ptrdiff_t pdiff;
+	int count = 0;
 
-/*
-	while (head)
+	while (head && (head - head->next > 0))
 	{
-		do
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			count++;
-			head = head->next;
-		} while (head - head->next > 0);
-	}
-*/
-
-
-	for (; head; count++)
-	{
-		pdiff = head - head->next;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (pdiff > 0)
-			head = head->next;
-		else
-		{
-			a = (head->next)->n;
-			printf("-> [%p] %d\n", (void *)head->next, a);
-			count++;
-			break;
-		}
+		count++;
+		head = head->next;
+	}
+	if (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		printf("-> [%p] %d\n", (void *)head->next, (head->next)->n);
+		count++;
 	}
 	return (count);
 }
