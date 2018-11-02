0c9c26546cebb56909f89c3bb0b02d84b19ffcfb
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-11-02 05:36:59 +0000

    Added files for Checks 0 and 1

---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef __HOLBERTON_H__
+#define __HOLBERTON_H__
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+
+#endif /* __HOLBERTON_H__ */
