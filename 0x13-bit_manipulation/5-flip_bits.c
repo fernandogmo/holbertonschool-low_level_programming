1dbd640ea19dd77da0e6e29081da64951bb96ca7
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-11-03 07:00:31 +0000

    Fixed betty and var type

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,14 @@
 /**
-  * flip_bits - returns the number of bits that must be flipped to get from one number to the another
+  * flip_bits - returns the number of bits that must be flipped
+  *		to get from one number to the another
   * @n: long uint source
   * @m: long uint destination
   * Return: uint count of bits flipped
   */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count = 0, diff = n ^ m;
+	unsigned int count = 0;
+	unsigned long int diff = n ^ m;
 
 	while (diff)
 	{
