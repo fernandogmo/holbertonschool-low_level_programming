eb81875d7bbbe96bdcc71e601aaec500e1a555f7
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-11-07 07:26:48 +0000

    Fixed typos and formatting for Checks 0 and 1

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -17,12 +17,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
+
 	r = read(fd, buf, letters);
 	if (r == -1)
 	{
@@ -30,6 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	close(fd);
+
 	w = write(STDOUT_FILENO, buf, r);
 	if (w < r)
 	{
--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -4,9 +4,9 @@
 #include <sys/stat.h>
 
 /**
-  * create_file - reads a text file and prints it to stdout
+  * create_file - creates a text file and writes some content
   * @filename: const string name of the file to create
-  * @text_content: string to add to file 
+  * @text_content: string to add to file
   * Return: 1 on success, -1 on failure
   */
 int create_file(const char *filename, char *text_content)
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | 0_CREAT | 0_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
