3a371a4c2670c78693710d0fdf67a7ceff977d0d
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-01-21 00:00:48 -0800

    Cleaned up. Still needs work.

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -7,25 +7,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *next = NULL;
+	hash_node_t *next = NULL, *prev = NULL;
 	unsigned long int i = 0;
 
 	if (ht && ht->array == NULL && ht->size)
 	{
 		for (; i < ht->size; i++)
 		{
-			while (ht->array[i] != NULL)
+			next = ht->array[i];
+			while (next)
 			{
-				next = ht->array[i]->next;
-				free(ht->array[i]->key);
-				free(ht->array[i]->value);
-				free(ht->array[i]);
-				ht->array[i] = next;
+				prev = next;
+				next = next->next;
+				free(prev->key);
+				free(prev->value);
+				free(prev);
 			}
 		}
 		free(ht->array);
-		ht->array = NULL;
-		ht->size = 0;
 		free(ht);
 	}
 }
